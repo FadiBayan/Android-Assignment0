18bc1fd9296364e72b8bfd39bf06c82ecda39413
FadiBayan <132906093+FadiBayan@users.noreply.github.com>
2025-09-05 03:25:49 +0300

    Initial commit - Assignment0 app in Java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,4 +1,18 @@
 package com.example.myapplication;
 
-public class MainActivity {
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find the TextView and set your name
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("Fadi Bayan");
+    }
 }
